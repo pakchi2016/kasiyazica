1184e6825cb10f59ae31115f361a4ff25def9ea9
pakchi2016 <pakchimax@hotmail.com>
2025-11-10 00:29:48 +0900

    画面作成

---
+++ kasiyazica/Form1.Designer.cs
@@ -0,0 +1,483 @@
+﻿namespace kasiyazica
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            groupBox1 = new GroupBox();
+            network = new Label();
+            exp = new Label();
+            stamina = new Label();
+            label5 = new Label();
+            power = new Label();
+            forging = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            groupBox2 = new GroupBox();
+            skill8 = new Label();
+            skill7 = new Label();
+            skill6 = new Label();
+            label13 = new Label();
+            skill5 = new Label();
+            label15 = new Label();
+            skill4 = new Label();
+            skill3 = new Label();
+            skill2 = new Label();
+            skill1 = new Label();
+            forging1 = new Button();
+            mining = new Button();
+            gird = new Button();
+            market = new Button();
+            bar = new Button();
+            court = new Button();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label6 = new Label();
+            label7 = new Label();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(55, 202);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(259, 169);
+            textBox1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Anchor = AnchorStyles.None;
+            groupBox1.BackColor = SystemColors.HighlightText;
+            groupBox1.Controls.Add(network);
+            groupBox1.Controls.Add(exp);
+            groupBox1.Controls.Add(stamina);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(power);
+            groupBox1.Controls.Add(forging);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.FlatStyle = FlatStyle.Flat;
+            groupBox1.Location = new Point(55, 33);
+            groupBox1.Name = "groupBox1";
+            groupBox1.RightToLeft = RightToLeft.No;
+            groupBox1.Size = new Size(129, 130);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "ステータス";
+            // 
+            // network
+            // 
+            network.BorderStyle = BorderStyle.FixedSingle;
+            network.Location = new Point(60, 89);
+            network.Margin = new Padding(0);
+            network.Name = "network";
+            network.Size = new Size(40, 20);
+            network.TabIndex = 2;
+            network.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // exp
+            // 
+            exp.BorderStyle = BorderStyle.FixedSingle;
+            exp.Location = new Point(60, 69);
+            exp.Margin = new Padding(0);
+            exp.Name = "exp";
+            exp.Size = new Size(40, 20);
+            exp.TabIndex = 2;
+            exp.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // stamina
+            // 
+            stamina.BorderStyle = BorderStyle.FixedSingle;
+            stamina.Location = new Point(60, 49);
+            stamina.Margin = new Padding(0);
+            stamina.Name = "stamina";
+            stamina.Size = new Size(40, 20);
+            stamina.TabIndex = 2;
+            stamina.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            label5.Location = new Point(60, 49);
+            label5.Margin = new Padding(0);
+            label5.Name = "label5";
+            label5.Size = new Size(40, 20);
+            label5.TabIndex = 2;
+            label5.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // power
+            // 
+            power.BorderStyle = BorderStyle.FixedSingle;
+            power.Location = new Point(60, 29);
+            power.Margin = new Padding(0);
+            power.Name = "power";
+            power.Size = new Size(40, 20);
+            power.TabIndex = 2;
+            power.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // forging
+            // 
+            forging.Location = new Point(60, 29);
+            forging.Margin = new Padding(0);
+            forging.Name = "forging";
+            forging.Size = new Size(40, 20);
+            forging.TabIndex = 2;
+            forging.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            label4.BorderStyle = BorderStyle.FixedSingle;
+            label4.Location = new Point(25, 89);
+            label4.Margin = new Padding(0);
+            label4.Name = "label4";
+            label4.Size = new Size(35, 20);
+            label4.TabIndex = 1;
+            label4.Text = "人脈";
+            label4.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            label3.BorderStyle = BorderStyle.FixedSingle;
+            label3.Location = new Point(25, 69);
+            label3.Margin = new Padding(0);
+            label3.Name = "label3";
+            label3.Size = new Size(35, 20);
+            label3.TabIndex = 1;
+            label3.Text = "経験";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            label2.BorderStyle = BorderStyle.FixedSingle;
+            label2.Location = new Point(25, 49);
+            label2.Margin = new Padding(0);
+            label2.Name = "label2";
+            label2.Size = new Size(35, 20);
+            label2.TabIndex = 1;
+            label2.Text = "体力";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            label1.BorderStyle = BorderStyle.FixedSingle;
+            label1.Location = new Point(25, 29);
+            label1.Margin = new Padding(0);
+            label1.Name = "label1";
+            label1.Size = new Size(35, 20);
+            label1.TabIndex = 0;
+            label1.Text = "筋力";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Anchor = AnchorStyles.None;
+            groupBox2.BackColor = SystemColors.HighlightText;
+            groupBox2.Controls.Add(skill8);
+            groupBox2.Controls.Add(skill7);
+            groupBox2.Controls.Add(skill6);
+            groupBox2.Controls.Add(label13);
+            groupBox2.Controls.Add(skill5);
+            groupBox2.Controls.Add(label15);
+            groupBox2.Controls.Add(skill4);
+            groupBox2.Controls.Add(skill3);
+            groupBox2.Controls.Add(skill2);
+            groupBox2.Controls.Add(skill1);
+            groupBox2.FlatStyle = FlatStyle.Flat;
+            groupBox2.Location = new Point(190, 33);
+            groupBox2.Name = "groupBox2";
+            groupBox2.RightToLeft = RightToLeft.No;
+            groupBox2.Size = new Size(124, 130);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "スキル";
+            // 
+            // skill8
+            // 
+            skill8.BorderStyle = BorderStyle.FixedSingle;
+            skill8.Location = new Point(62, 89);
+            skill8.Margin = new Padding(0);
+            skill8.Name = "skill8";
+            skill8.Size = new Size(40, 20);
+            skill8.TabIndex = 2;
+            skill8.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // skill7
+            // 
+            skill7.BorderStyle = BorderStyle.FixedSingle;
+            skill7.Location = new Point(62, 69);
+            skill7.Margin = new Padding(0);
+            skill7.Name = "skill7";
+            skill7.Size = new Size(40, 20);
+            skill7.TabIndex = 2;
+            skill7.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // skill6
+            // 
+            skill6.BorderStyle = BorderStyle.FixedSingle;
+            skill6.Location = new Point(62, 49);
+            skill6.Margin = new Padding(0);
+            skill6.Name = "skill6";
+            skill6.Size = new Size(40, 20);
+            skill6.TabIndex = 2;
+            skill6.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label13
+            // 
+            label13.Location = new Point(62, 49);
+            label13.Margin = new Padding(0);
+            label13.Name = "label13";
+            label13.Size = new Size(40, 20);
+            label13.TabIndex = 2;
+            label13.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // skill5
+            // 
+            skill5.BorderStyle = BorderStyle.FixedSingle;
+            skill5.Location = new Point(62, 29);
+            skill5.Margin = new Padding(0);
+            skill5.Name = "skill5";
+            skill5.Size = new Size(40, 20);
+            skill5.TabIndex = 2;
+            skill5.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label15
+            // 
+            label15.Location = new Point(62, 29);
+            label15.Margin = new Padding(0);
+            label15.Name = "label15";
+            label15.Size = new Size(40, 20);
+            label15.TabIndex = 2;
+            label15.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // skill4
+            // 
+            skill4.BorderStyle = BorderStyle.FixedSingle;
+            skill4.Location = new Point(22, 89);
+            skill4.Margin = new Padding(0);
+            skill4.Name = "skill4";
+            skill4.Size = new Size(40, 20);
+            skill4.TabIndex = 1;
+            skill4.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // skill3
+            // 
+            skill3.BorderStyle = BorderStyle.FixedSingle;
+            skill3.Location = new Point(22, 69);
+            skill3.Margin = new Padding(0);
+            skill3.Name = "skill3";
+            skill3.Size = new Size(40, 20);
+            skill3.TabIndex = 1;
+            skill3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // skill2
+            // 
+            skill2.BorderStyle = BorderStyle.FixedSingle;
+            skill2.Location = new Point(22, 49);
+            skill2.Margin = new Padding(0);
+            skill2.Name = "skill2";
+            skill2.Size = new Size(40, 20);
+            skill2.TabIndex = 1;
+            skill2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // skill1
+            // 
+            skill1.BorderStyle = BorderStyle.FixedSingle;
+            skill1.Location = new Point(22, 29);
+            skill1.Margin = new Padding(0);
+            skill1.Name = "skill1";
+            skill1.Size = new Size(40, 20);
+            skill1.TabIndex = 0;
+            skill1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // forging1
+            // 
+            forging1.Location = new Point(370, 33);
+            forging1.Margin = new Padding(0);
+            forging1.Name = "forging1";
+            forging1.Size = new Size(120, 40);
+            forging1.TabIndex = 2;
+            forging1.Text = "鍛造";
+            forging1.UseVisualStyleBackColor = true;
+            forging1.Click += forging1_Click;
+            // 
+            // mining
+            // 
+            mining.Location = new Point(370, 153);
+            mining.Margin = new Padding(0);
+            mining.Name = "mining";
+            mining.Size = new Size(120, 40);
+            mining.TabIndex = 2;
+            mining.Text = "採掘";
+            mining.UseVisualStyleBackColor = true;
+            // 
+            // gird
+            // 
+            gird.Location = new Point(370, 213);
+            gird.Margin = new Padding(0);
+            gird.Name = "gird";
+            gird.Size = new Size(120, 40);
+            gird.TabIndex = 2;
+            gird.Text = "ギルド";
+            gird.UseVisualStyleBackColor = true;
+            // 
+            // market
+            // 
+            market.Location = new Point(370, 93);
+            market.Margin = new Padding(0);
+            market.Name = "market";
+            market.Size = new Size(120, 40);
+            market.TabIndex = 2;
+            market.Text = "市場";
+            market.UseVisualStyleBackColor = true;
+            // 
+            // bar
+            // 
+            bar.Location = new Point(370, 273);
+            bar.Margin = new Padding(0);
+            bar.Name = "bar";
+            bar.Size = new Size(120, 40);
+            bar.TabIndex = 2;
+            bar.Text = "酒場";
+            bar.UseVisualStyleBackColor = true;
+            // 
+            // court
+            // 
+            court.Location = new Point(370, 333);
+            court.Margin = new Padding(0);
+            court.Name = "court";
+            court.Size = new Size(120, 40);
+            court.TabIndex = 2;
+            court.Text = "宮廷";
+            court.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(543, 273);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(219, 109);
+            textBox2.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(543, 93);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(219, 109);
+            textBox3.TabIndex = 0;
+            // 
+            // label6
+            // 
+            label6.BackColor = SystemColors.HighlightText;
+            label6.Location = new Point(543, 70);
+            label6.Margin = new Padding(0);
+            label6.Name = "label6";
+            label6.Size = new Size(35, 20);
+            label6.TabIndex = 0;
+            label6.Text = "依頼";
+            label6.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            label7.BackColor = SystemColors.HighlightText;
+            label7.Location = new Point(543, 250);
+            label7.Margin = new Padding(0);
+            label7.Name = "label7";
+            label7.Size = new Size(35, 20);
+            label7.TabIndex = 0;
+            label7.Text = "素材";
+            label7.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(gird);
+            Controls.Add(mining);
+            Controls.Add(market);
+            Controls.Add(court);
+            Controls.Add(bar);
+            Controls.Add(forging1);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(textBox3);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private GroupBox groupBox1;
+        private Label label1;
+        private Label label2;
+        private Label label4;
+        private Label label3;
+        private Label network;
+        private Label exp;
+        private Label stamina;
+        private Label label5;
+        private Label power;
+        private Label forging;
+        private GroupBox groupBox2;
+        private Label skill8;
+        private Label skill7;
+        private Label skill6;
+        private Label label13;
+        private Label skill5;
+        private Label label15;
+        private Label skill4;
+        private Label skill3;
+        private Label skill2;
+        private Label skill1;
+        private Button forging1;
+        private Button mining;
+        private Button gird;
+        private Button market;
+        private Button bar;
+        private Button court;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label6;
+        private Label label7;
+    }
+}
---
+++ kasiyazica/Form1.cs
@@ -0,0 +1,16 @@
+namespace kasiyazica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void forging1_Click(object sender, EventArgs e)
+        {
+            forging forging = new forging();
+            forging.Show();
+        }
+    }
+}
